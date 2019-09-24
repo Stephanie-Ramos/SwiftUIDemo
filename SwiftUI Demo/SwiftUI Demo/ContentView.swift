b70b81dd16e82ad08d4c2f6c0cb23a222dcd9108
Stephanie Ramos <stephanien.ramos@Stephanies-MacBook-Pro-2.local>
2019-09-24 15:51:57 -0400

    added xcode project

---
+++ SwiftUI Demo/SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Stephanie Ramos on 9/24/19.
+//  Copyright © 2019 Stephanie N. Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
